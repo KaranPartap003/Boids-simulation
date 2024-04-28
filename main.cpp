93af180948429963e9259796ac69b20d7f4a0574
Karan Partap <122453969+KaranPartap003@users.noreply.github.com>
2024-04-28 17:50:07 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,13 +12,19 @@
 int PARTICLE_COUNT = 19200;
 
 //-----------UI VARIABLES-------------//
-GLfloat viewRadius, avoidRadius, cohesionWeight, alignmentWeight, separationWeight, boundary;
-glm::vec3 Maxbndry = { 1.0f, 1.0f, 1.0f };
+GLfloat viewRadius, avoidRadius, cohesionWeight, 
+        alignmentWeight, separationWeight, boundary;
+
+glm::vec3 Maxbndry = { 1.0f, 1.0f ,1.0f};
 glm::vec3 Minbndry = {-1.0f,-1.0f,-1.0f };
 
-GLuint dt, n, Maxlimit, Minlimit ,view, avoid, cohesion, align, seperation, fov;
+GLuint dt, n, Maxlimit, Minlimit,
+       view, avoid, cohesion, align, seperation, fov;
 
-GLuint position_buffer, velocity_buffer, color_buffer, position_tex, velocity_tex, color_tex, VAO;
+GLuint position_buffer, velocity_buffer, color_buffer, 
+       currPos, currVel, currCol, 
+       prevPos, prevVel, prevCol,VAO;
+
 Shader shader, compute;
 
 GLint numOfWorkgroups = PARTICLE_COUNT / 128;
@@ -30,8 +36,6 @@ Camera camera;
 
 const float aspRatio = 1366 / 768;
 const float toRadians = 3.14159265f / 180.0f;
-
-std::vector<int> attractor_masses(numOfAttractors);
 
 void setUniforms(GLuint ID, double deltaTime)
 {
@@ -49,13 +53,13 @@ void setUniforms(GLuint ID, double deltaTime)
     view = glGetUniformLocation(ID, "viewRadius");
     glUniform1f(view, 0.5);
     avoid = glGetUniformLocation(ID, "avoidRadius");
-    glUniform1f(avoid, 0.01);
+    glUniform1f(avoid, 0.005);
     cohesion = glGetUniformLocation(ID, "cohesionWeight");
-    glUniform1f(cohesion, 0.6);
+    glUniform1f(cohesion, 0.7);
     align = glGetUniformLocation(ID, "alignmentWeight");
-    glUniform1f(align, 0.45);
+    glUniform1f(align, 0.4);
     seperation= glGetUniformLocation(ID, "separationWeight");
-    glUniform1f(seperation, 0.05);
+    glUniform1f(seperation, 1.5);
 }
 
 void ComputeShaderPass(double deltaTime)
@@ -64,10 +68,14 @@ void ComputeShaderPass(double deltaTime)
 
     setUniforms(compute.GetID(), deltaTime);
 
-    // Bind position and velocity buffers to image units
-    glBindImageTexture(0, position_tex, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
-    glBindImageTexture(1, velocity_tex, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
-    glBindImageTexture(2, color_tex, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+    // Bind curr position, color and velocity buffers to image units
+    glBindImageTexture(0, currPos, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+    glBindImageTexture(1, currVel, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+    glBindImageTexture(2, currCol, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+    // prev buffers
+    glBindImageTexture(3, prevPos, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+    glBindImageTexture(4, prevVel, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+    glBindImageTexture(5, prevCol, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
     // Dispatch compute shader
     glDispatchCompute(numOfWorkgroups, 1, 1);
 
@@ -79,15 +87,11 @@ void RenderPass(double deltaTime)
 {
     shader.UseShader();
     //setup mvp matrix
-    glm::mat4 model = glm::mat4(1.0f);
-    glm::mat4 perspective = glm::perspective(glm::radians(60.0f), (GLfloat)mainWindow.GetBufferWidth() / mainWindow.GetBufferHeight(), 0.1f, 100.0f);
-    glm::mat4 view = camera.calculateViewMatrix();
-    int m = glGetUniformLocation(shader.GetID(), "model");
-    int v = glGetUniformLocation(shader.GetID(), "view");
-    int p = glGetUniformLocation(shader.GetID(), "perspective");
-    glUniformMatrix4fv(m, 1, GL_FALSE, glm::value_ptr(model));
-    glUniformMatrix4fv(v, 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(p, 1, GL_FALSE, glm::value_ptr(perspective));
+    glm::mat4 mvp = glm::mat4(1.0f);
+    mvp *= glm::perspective(glm::radians(60.0f), (GLfloat)mainWindow.GetBufferWidth() / mainWindow.GetBufferHeight(), 0.1f, 100.0f);
+    mvp *= camera.calculateViewMatrix();
+    int m = glGetUniformLocation(shader.GetID(), "mvp");
+    glUniformMatrix4fv(m, 1, GL_FALSE, glm::value_ptr(mvp));
     // Bind VAO and issue draw commands
     glBindVertexArray(VAO);
     glEnable(GL_BLEND);
@@ -133,9 +137,9 @@ void InitialiseBuffers()
     for (int i = 0; i < PARTICLE_COUNT; ++i)
     {
         // Generate random values for each component using glm::linearRand()
-        float r = glm::linearRand(0.5f, 1.0f);
-        float g = glm::linearRand(0.5f, 1.0f);
-        float b = glm::linearRand(0.5f, 1.0f);
+        float r = glm::linearRand(0.0f, 1.0f);
+        float g = glm::linearRand(0.0f, 1.0f);
+        float b = glm::linearRand(0.0f, 1.0f);
         colors[i] = glm::vec4(r, g, b, 1.0f);
     }
     glUnmapBuffer(GL_ARRAY_BUFFER);
@@ -147,19 +151,29 @@ void InitialiseBuffers()
 
     //bind posiiton buffer as texture
     glBindBuffer(GL_TEXTURE_BUFFER, position_buffer);
-    //generate position_tex
-    glGenTextures(1, &position_tex);
-    glBindTexture(GL_TEXTURE_BUFFER, position_tex);
+    //generate currPos
+    glGenTextures(1, &currPos);
+    glBindTexture(GL_TEXTURE_BUFFER, currPos);
     glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, position_buffer);
     glBindTexture(GL_TEXTURE_BUFFER, 0);
-    //bind color buffer as texture
-    glBindBuffer(GL_TEXTURE_BUFFER, color_buffer);
-    //generate color_tex
-    glGenTextures(1, &color_tex);
-    glBindTexture(GL_TEXTURE_BUFFER, color_tex);
-    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, color_buffer);
+    //generate prevPos
+    glGenTextures(1, &prevPos);
+    glBindTexture(GL_TEXTURE_BUFFER, prevPos);
+    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, position_buffer);
     glBindTexture(GL_TEXTURE_BUFFER, 0);
 
+    //bind color buffer as texture
+    glBindBuffer(GL_TEXTURE_BUFFER, color_buffer);
+    //generate currCol
+    glGenTextures(1, &currCol);
+    glBindTexture(GL_TEXTURE_BUFFER, currCol);
+    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, color_buffer);
+    glBindTexture(GL_TEXTURE_BUFFER, 0);
+    //prev Color
+    glGenTextures(1, &prevCol);
+    glBindTexture(GL_TEXTURE_BUFFER, prevCol);
+    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, color_buffer);
+    glBindTexture(GL_TEXTURE_BUFFER, 0);
 
     //INITIALISING VELOCITIES
     glGenBuffers(1, &velocity_buffer);
@@ -171,17 +185,24 @@ void InitialiseBuffers()
     for (int i = 0; i < PARTICLE_COUNT; ++i)
     {
         // Generate random values for each component using glm::linearRand()
-        float x = glm::linearRand(-0.1f,0.1f);
-        float y = glm::linearRand(-0.1f,0.1f);
-        float z = glm::linearRand(-0.1f,0.1f);
+        float x = glm::linearRand(-0.04f,0.04f);
+        float y = glm::linearRand(-0.04f,0.04f);
+        float z = glm::linearRand(-0.04f,0.04f);
         velocities[i] = glm::vec4(x, y, z, 0);
     }
     glUnmapBuffer(GL_TEXTURE_BUFFER);
-    glGenTextures(1, &velocity_tex);
-    glBindTexture(GL_TEXTURE_BUFFER, velocity_tex);
+    //curr Velocity
+    glGenTextures(1, &currVel);
+    glBindTexture(GL_TEXTURE_BUFFER, currVel);
+    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, velocity_buffer);
+    glBindTexture(GL_TEXTURE_BUFFER, 0);
+    //prev Velocity
+    glGenTextures(1, &prevVel);
+    glBindTexture(GL_TEXTURE_BUFFER, prevVel);
     glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA32F, velocity_buffer);
     glBindTexture(GL_TEXTURE_BUFFER, 0);
 
+    glBindBuffer(0, GL_TEXTURE_BUFFER);
 }
 
 int main()
@@ -215,11 +236,14 @@ int main()
         camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange(), deltaTime);
 
         /* Render here */
-        glClearColor(0.1f, 0.1f, 0.1f, 0.5);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.5);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         ComputeShaderPass(deltaTime);
         RenderPass(deltaTime);
+        std::swap(prevCol, currCol);
+        std::swap(prevVel, currVel);
+        std::swap(prevPos, currPos);
 
         // Unbind shader program
         glUseProgram(0);
@@ -231,4 +255,3 @@ int main()
     glfwTerminate();
     return 0;
 }
-
